f8d1c71f50748a0bc391710f82be0b86707d70d8
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2021-02-07 16:01:04 +0000

    Added larger screen nav, dropdown menu on nav

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -2,6 +2,7 @@ const navToggle = () => {
   const navBtn = document.getElementById('navBtn');
   const navOverlay = document.getElementById('navOverlay');
   const navItem = document.querySelectorAll('.nav-item');
+  const navDropdown = document.getElementById('navDropdown');
 
   navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('active');
@@ -18,6 +19,12 @@ const navToggle = () => {
       document.body.classList.toggle('nav-overlay-noscroll');
     })
   );
+
+  navDropdown.addEventListener('click', () => {
+    const dropdownLinks = document.getElementById('dropdownLinks');
+    dropdownLinks.classList.toggle('hidden');
+    dropdownLinks.classList.toggle('dropdown-links-show');
+  });
 };
 
 export default navToggle;
